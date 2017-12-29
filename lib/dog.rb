2b1ae2c751c53e438f7b0e91a92f1dcfc6c0bf37
Meredith Reece <mardebugger@gmail.com>
2017-12-29 02:53:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,14 +31,22 @@ class Dog
     dog = self.new(id:array[0], name:array[1], breed:array[2])
   end
 
-  def self.find_by_name(name)
-    find_name_sql = <<-SQL
-    SELECT * FROM dogs
-    WHERE name = ?;
+  # def self.find_by_name(name)
+  #   find_name_sql = <<-SQL
+  #   SELECT * FROM dogs
+  #   WHERE name = ?;
+  #   SQL
+  #   DB[:conn].execute(find_name_sql, name).collect do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end
+
+  def save
+    save_sql = <<-SQL
+    INSERT INTO dogs IF NOT EXISTS (name, breed)
+    VALUES (?, ?)
     SQL
-    DB[:conn].execute(find_name_sql, name).collect do |row|
-      self.new_from_db(row)
-    end.first
+    DB[:conn].execute(save_sql, self.name, self.breed)
   end
 
 end
